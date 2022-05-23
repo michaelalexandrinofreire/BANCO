bce7ba1d6efea663c16c4ed12c37e4144e3b7347
Michael <104912294+michaelalexandrinofreire@users.noreply.github.com>
2022-05-23 09:58:51 -0300

    melhoria de código e testes

--- banco.py
+++ banco.py
@@ -3,22 +3,20 @@ from ast import Break
 
 #INÍCIO
 p = "-" * 20
+saldo1 = 1200
+
 print(p)
 print("BANCO ULTRA")
 print(p)
 
-#VARIÁVEIS
-saldo1 = 1250
-cont = 0
-
 #FUNÇÕES
-def saque():
+def saque(): #PROBLEMA: VALORES ALTOS NÃO SÃO EFETUADOS NO SAQUE(EX: 900)
     global saldo1
     saque1 = float(input("VALOR DO SAQUE: "))
     
     if saque1 > saldo1:
-        print("VALOR MÁXIMO PARA SAQUE DE: ", saldo1)
-        return chamaFuncao
+        print("VALOR MÁXIMO PARA SAQUE DE: {:.2f} ".format(saldo1))
+        return main()
     
     saldo1 -= saque1
     if saque1 <= saldo1:
@@ -26,7 +24,7 @@ def saque():
         return menuSaque()
 
 def saldo():
-    print("SEU SALDO É DE: ", saldo1)
+    print("SEU SALDO É DE: {:.2f} ".format(saldo1))
     print(p)
     return menuSaldo()
 
@@ -34,36 +32,32 @@ def menuSaque():
     a = int(input("PARA CONSULTAR SEU NOVO SALDO: DIGITE 1 \n" "PARA VOLTAR AO MENU: DIGITE 2 \n"))
     print(p)
     if a == 1:
-        print("SEU NOVO SALDO É DE:", saldo1) 
+        print("SEU NOVO SALDO É DE: {:.2f}".format(saldo1)) 
         print(p)
-        return chamaFuncao()
+        return main()
     if a == 2:
-        return chamaFuncao()
+        return main()
     else:
-        print("Número inválido!")
-        return chamaFuncao()
-
+        print("Número ------o!")
+        return main()
 def menuSaldo():
+    a = int(input("PARA VOLTAR AO MENU: DIGITE 1 \n" "PARA ENCERRAR O PROGRAMA: DIGITE 2 \n"))
+    print(p)
     for i in range(1):
-        a = int(input("PARA VOLTAR AO MENU: DIGITE 1 \n" "PARA ENCERRAR O PROGRAMA: DIGITE 2 \n"))
-        print(p)
         if a == 1:
-            return chamaFuncao()
+            return main()
         if a == 2:
             break
-        else:
-            print("Número inválido!")
-            return chamaFuncao()
-
-def chamaFuncao():
-    b = int(input("PARA CONSULTAR SEU SALDO: DIGITE 1 \n" "PARA REALIZAR UM SAQUE: DIGITE 2 \n"))
-    if b == 1:
-        saldo()
-    if b == 2:
-        saque()
-    else:
-        print("NÚMERO INVÁLIDO!")
-        return chamaFuncao()
         
-#CHAMANDO AS FUNÇÕES
-chamaFuncao()
+def main():
+    b = int(input("PARA CONSULTAR SEU SALDO: DIGITE 1 \n" "PARA REALIZAR UM SAQUE: DIGITE 2 \n" "PARA ENCERRAR O PROGRAMA: DIGITE 3\n"))
+    for i in range(1):
+        if b == 1:
+            saldo()
+        if b == 2:
+            saque()
+        if b == 3:
+            break
+        
+#CHAMANDO FUNÇÃO PRINCIPAL
+main()
